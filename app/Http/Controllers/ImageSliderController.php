2ae3bb2a1f819f2fa5c34de7720112547851f321
miladmohammadi82 <afraaliansite@gmail.com>
2021-06-12 22:43:17 +0430

    create files for imageslider

---
+++ app/Http/Controllers/ImageSliderController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ImageSlider;
+use Illuminate\Http\Request;
+
+class ImageSliderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ImageSlider  $imageSlider
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ImageSlider $imageSlider)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ImageSlider  $imageSlider
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ImageSlider $imageSlider)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ImageSlider  $imageSlider
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ImageSlider $imageSlider)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ImageSlider  $imageSlider
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ImageSlider $imageSlider)
+    {
+        //
+    }
+}
