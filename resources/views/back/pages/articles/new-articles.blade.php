2d901ca4eec3c0453e916b91d1b90eb165562e09
Milad Mohammadi <afraaliansite@gmail.com>
2021-06-24 08:06:53 -0700

    inestall and fix bug in lfm pakage

--- resources/views/back/pages/articles/new-articles.blade.php
+++ resources/views/back/pages/articles/new-articles.blade.php
@@ -76,12 +76,17 @@
                         <div class="input-fild-box form-group">
                             <label for="">تصویر شاخص</label>
                             <div class="input-group">
-                                <input type="file" name="index_image" id="">
+                                <span class="input-group-btn">
+                                <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
+                                    <i class="fa fa-picture-o"></i> انتخاب
+                                </a>
+                                </span>
+                                <input id="thumbnail" value="{{ $articles->index_image }}" @error('index_image') is-invalid @enderror class="form-control" type="text" name="index_image">
                             </div>
+                            <img id="holder" src="{{ $articles->index_image }}" style="margin-top:15px;max-height:100px;" />
                             @error('index_image')
                                 <div class="text-danger">{{ $message }}</div>
                             @enderror
-                        </div>
 
                         <div class="input-fild-box form-group">
                             <button type="submit" class="btn btn-success w-100">ارسال</button>
