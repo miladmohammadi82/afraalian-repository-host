45fccee1ef05a819994b437c6b4d32c4be13df1a
Milad Mohammadi <afraaliansite@gmail.com>
2021-09-10 08:13:36 -0700

    ok

---
+++ resources/js/Pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+    <layout>
+        <h1>About</h1>
+    </layout>
+</template>
+
+<script>
+import Layout from '../layout.vue'
+export default {
+    components:{
+        Layout
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/Pages/contact.vue
@@ -0,0 +1,18 @@
+<template>
+    <layout>
+        <h1>Contact</h1>
+    </layout>
+</template>
+
+<script>
+import Layout from '../shared/layout.vue'
+export default {
+    components:{
+        Layout
+    }
+}
+</script>
+
+<style>
+
+</style>
